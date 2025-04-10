afe50a2434969784d701397c49f68fadd295c355
[ sora. ] <57730505+GoodZerg@users.noreply.github.com>
2025-04-10 18:45:49 +0300

    gitignore update + init proj

---
+++ TetrisCV/TetrisCV.cpp
@@ -0,0 +1,17 @@
+﻿#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <iostream>
+using namespace cv;
+
+int main()
+{
+    cv::Mat img = cv::imread("C:\\Insaid\\GAME_ALL\\Art\\pint\\b469677fa5c5fd9880cf4ca092b4392f.jpg");
+    namedWindow("First OpenCV Application", WINDOW_AUTOSIZE);
+
+    cv::imshow("First OpenCV Application", img);
+    cv::moveWindow("First OpenCV Application", 0, 45);
+    cv::waitKey(0);
+    cv::destroyAllWindows();
+    return 0;
+}
